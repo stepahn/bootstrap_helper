90ade3e862547741b21ebb6a99b579255ab2f536
Stephan Zalewski <stephan@foo-labs.com>
2013-10-24 10:20:38 +0200

    added description list helper

--- lib/bootstrap_helper/view_helper.rb
+++ lib/bootstrap_helper/view_helper.rb
@@ -1,4 +1,17 @@
 module BootstrapHelper
   module ViewHelper
+    def bdesc
+      content_tag :dl, class: 'dl-horizontal' do
+        yield
+      end
+    end
+
+    def bdesc_entry(label, value)
+      if value.blank?
+        value = '&nbsp;'.html_safe
+      end
+      content_tag(:dt, label) + content_tag(:dd, value)
+    end
+
   end
 end
